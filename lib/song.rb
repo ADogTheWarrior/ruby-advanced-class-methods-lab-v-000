041a23b265317bf165228d342678a137546f4398
Adam Fudge <coolfudge@gmail.com>
2017-10-15 19:10:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,37 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    placeholder = self.new
+    @@all << placeholder
+    return placeholder
+  end
+
+  def self.new_by_name(name)
+    # placeholder = self.new
+    # placeholder.name = name
+    # @@all << placeholder
+  end
+
+  def self.create_by_name(name)
+  end
+
+  def self.find_by_name(name)
+  end
+
+  def self.find_or_create_by_name(name)
+  end
+
+  def self.alphabetical
+  end
+
+  def self.new_from_filename(name)
+  end
+
+  def self.create_from_filename(name)
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
